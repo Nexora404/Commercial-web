923e9ae58e2a20c0c6407406fedfd85280e43c8e
Nexora <Peter3ondiek@gmail.com>
2025-11-15 11:24:53 +0300

    Disable JSON response logging
    Comment out JSON response handling in promise chain

--- Commercial.js
+++ Commercial.js
@@ -154,5 +154,5 @@ fetch("https://jsonplaceholder.typicode.com/posts",
     }
 }
 )
-.then(response => response.json());
-.then(json=> console.log(json));
+//.then(response => response.json());
+//.then(json=> console.log(json));
